01c80ed73f2dce17d8bbdcacabdc92e9be6f3b73
Steve Gilroy <shtev@inspiraldreams.com>
2016-02-12 15:16:40 +0000

    settings tweaks

--- wagtaildemo/settings/heroku.py
+++ wagtaildemo/settings/heroku.py
@@ -15,6 +15,21 @@ DATABASES = {'default': dj_database_url.config(default='postgres://postgres@loca
 
 INSTALLED_APPS += ['storages',]
 AWS_STORAGE_BUCKET_NAME = 'wagtail-demo'
+AWS_STORAGE_BUCKET_REGION = os.environ['AWS_STORAGE_BUCKET_REGION']
+if '.' in AWS_STORAGE_BUCKET_NAME:
+    from boto.s3.connection import OrdinaryCallingFormat
+
+    class RegionPathCallingFormat(OrdinaryCallingFormat):
+
+        def get_bucket_server(self, server, bucket):
+            if not bucket:
+                return OrdinaryCallingFormat.get_bucket_server(
+                    self, server, bucket)
+            return 's3-%s.amazonaws.com' % (AWS_STORAGE_BUCKET_REGION,)
+
+    AWS_S3_CALLING_FORMAT = RegionPathCallingFormat()
+
+AWS_LOCATION = os.environ.get('AWS_LOCATION', '')
 DEFAULT_FILE_STORAGE = 'storages.backend.s3boto.S3BotoStorage'
 MEDIA_URL = ''
 
