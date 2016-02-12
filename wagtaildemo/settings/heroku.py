1dadbd3ebaf2be5e5e1818972188eebae4a0c9a1
Steve Gilroy <shtev@inspiraldreams.com>
2016-02-12 15:05:33 +0000

    settings tweaks

--- wagtaildemo/settings/heroku.py
+++ wagtaildemo/settings/heroku.py
@@ -13,12 +13,10 @@ BASE_URL = 'http://localhost:8000'
 
 DATABASES = {'default': dj_database_url.config(default='postgres://postgres@localhost:5432/wagtaildemo')}
 
-AWS_STORAGE_BUCKET_NAME = "wagtail-demo"
+INSTALLED_APPS += ['storages',]
+AWS_STORAGE_BUCKET_NAME = 'wagtail-demo'
 DEFAULT_FILE_STORAGE = 'storages.backend.s3boto.S3BotoStorage'
-MEDIA_URL = "http://%s.s3.amazonaws.com/" % AWS_STORAGE_BUCKET_NAME
-
-MEDIA_ROOT = '/media/'
-STATIC_ROOT = '/static/'
+MEDIA_URL = ''
 
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
